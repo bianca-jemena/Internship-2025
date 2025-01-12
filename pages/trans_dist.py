1d93d44d81a19e3c6d2464bff5d9d05b23cb2c19
eylim-jem <eyyern.lim@jemena.com.au>
2025-01-13 10:38:23 +1100

    Added more info, images

--- pages/trans_dist.py
+++ pages/trans_dist.py
@@ -25,6 +25,7 @@ st.title("What is an Electricity Distribution Network?")
 
 st.write(
     """
+    Distribution is one of the stages of the electricity network, coming after the Generation and Transmsission stages.
     An **electricity distribution network** is the portion of the power delivery system 
     that carries electricity from higher-voltage transmission lines to homes, businesses, 
     and other end users at a lower voltage level.
@@ -64,9 +65,17 @@ st.header("What does Jemena do?")
 st.write(
     """
     Distribution is Jemena’s position in the supply chain (a **DNSP** or Distribution Network Service Provider). In Victoria, Jemena covers the electricity needs of a variety of different types of customers. 
-    This includes residential and industrial areas, small businesses and even Melbourne International Airport. To do so, Jemena owns and operates important infrastructure such as these terminal stations, transmission lines and many different types of transformers. Distribution involves carrying electricity locally from terminal stations to businesses and homes. This electricity is transported via overhead power lines, and is stepped down again via smaller transformers before being sent into customers’ homes as the commonly used 230V standard.
+    This includes residential and industrial areas, small businesses and even Melbourne International Airport. To do so, Jemena owns and operates important infrastructure such as these terminal stations, 
+    transmission lines and many different types of transformers. Distribution involves carrying electricity locally from terminal stations to businesses and homes. 
+    This electricity is transported via overhead power lines, and is stepped down again via smaller transformers before being sent into customers’ homes as the commonly used 230V standard.
     """
 )
+
+with st.expander("Did you know?"):
+    # st.write('''
+    #     Jemena maintains more than 107,000 poles and their wires, with approximately 78,500 public lights under our service.
+    #          ''')
+    st.image(r"C:\Users\eylim\OneDrive - onlineeportal\Documents\GitHub\Internship-2025\assets\JEM_facts.png")
 st.write(
     """
     As a distribution network service provider (DNSP), Jemena is responsible for building, maintaining and operating its distribution network. To ensure the quality of service, Jemena is responsible for following the:
@@ -78,15 +87,18 @@ st.write(
 )
 st.write(
     """
-    Additionally, as a distribution network, Jemena is required to meet reliability standards:
+    Additionally, as a distribution network, Jemena is required to meet reliability standards to manage network limitations:
 
-    - Network limitations (thermal, voltage and fault level)
-    - Thermal: asset utilisation must be managed in order to ensure that there are no overloads (potential asset failures or electrical safety clearance violations)
-    - Voltage: Excessively high or low voltages result in accelerated loss of life in customer appliances or other safety issues
-    - Fault level: prevent switchgear replacement, conductor and cable sizes must be adequately selected to withstand fault level requirements
-    - Redundancy in the case of Emergency or maintenance.
+    - **Thermal**: asset utilisation must be managed in order to ensure that there are no overloads (potential asset failures or electrical safety clearance violations)
+    - **Voltage**: Excessively high or low voltages result in accelerated loss of life in customer appliances or other safety issues
+    - **Fault level**: prevent switchgear replacement, conductor and cable sizes must be adequately selected to withstand fault level requirements
+    - **Redundancy**: In the case of emergency or maintenance, following a "n-1" rule in network planning allows for planned or unexpected failures to have less impact on network stability.
     """
 )
+with st.expander("What is n-1?"):
+    st.write('''
+            A n-1 redundancy system in the context of the electricity network is one in which there is a backup route of electricity delivery should any component along any single network line fail.
+             ''')
 st.write(
     """
     Combined, these reliability and quality standards ensure that the grid is resilient enough to manage the load and support current and potentially forecasted customer demands
@@ -97,6 +109,14 @@ st.write(
 
     """
 )
+
+st.header("Where does Jemena service?")
+st.write('''
+        Jemena's disribution area extends over the North-West of Melbourne. This distibution area covers a mix of industrial, commercial and residential customers,
+        some major transport routes as well as the Melbourne International Airport. Semi-rural areas (though rapidly developing) exist around Sydenham, Sunbury and Coolaroo.
+        The Jemena Network environment is bordered by the Yarra river in the East and Port Phillip Bay in the south.
+        ''')
+st.image(r"C:\Users\eylim\OneDrive - onlineeportal\Documents\GitHub\Internship-2025\assets\JEM_network.jpg", caption="The Jemena Electricity Network")
 # ---------------------------
 # Interactive Quiz / Elements
 # ---------------------------
@@ -109,15 +129,30 @@ if st.checkbox("Transmission"):
 if st.checkbox("Distribution"):
     st.error("Distribution lines carry electricity at lower voltages to the end users.")
 
-# 2. Buttons question
-st.write("**Q2: Which segment of the network typically operates at 120 V or 240 V for homes?**")
+st.write("**Q2: Approximately how many customers do you think Jemena services?**")
 answer_q2 = st.radio(
+    "",
+    ["100,000", "200,000", "300,000", "400,000"]
+)
+if answer_q2==("100,000"):
+    st.error("Jemena services more customers than this!")
+elif answer_q2==("200,000"):
+    st.error("Jemena services more customers than this!")
+elif answer_q2 == ("300,000"):
+    st.error("Jemena services more customers than this!")
+elif answer_q2==("400,000"):
+    st.success("Correct! Jemena services more than 387,000 customers for their electricity needs!")
+    st.image(r"C:\Users\eylim\OneDrive - onlineeportal\Documents\GitHub\Internship-2025\assets\JEN_customers.png", caption="The Jemena Customer Breakdown")
+
+# 2. Buttons question
+st.write("**Q3: Which segment of the network typically operates at 120 V or 240 V for homes?**")
+answer_q3 = st.radio(
     "",
     ["Transmission Network", "Distribution Network"]
 )
-if answer_q2 == "Transmission Network":
+if answer_q3 == "Transmission Network":
     st.error("Not quite. Transmission lines operate at much higher voltages.")
-elif answer_q2 == "Distribution Network":
+elif answer_q3 == "Distribution Network":
     st.success("Correct! Distribution lines usually serve customers at these lower voltage levels.")
 
 # 3. Slider to guess typical medium voltage range
